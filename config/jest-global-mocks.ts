6a79355fd15ea7e70ab93df9ab8d27441e7ca8bc
Huang Jian <huang.jian@howtimeflies.io>
2018-02-12 16:23:27 +0800

    Replace Karma+Jasmine with Jest.

---
+++ config/jest-global-mocks.ts
@@ -0,0 +1,18 @@
+const mock = () => {
+  let store = {}
+  return {
+    getItem(key) {
+      return store[key]
+    },
+    setItem(key, value) {
+      store[key] = value.toString()
+    },
+    removeItem(key) {
+      delete store[key]
+    },
+    clear() {
+      store = {}
+    }
+  }
+}
+Object.defineProperty(window, 'localStorage', {value: mock()})
---
+++ config/jest.ts
@@ -0,0 +1,5 @@
+import 'jest-preset-angular'
+import 'rxjs/Rx'
+import 'zone.js/dist/long-stack-trace-zone'
+
+import './jest-global-mocks'
