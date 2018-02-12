ae8176628d36a4640bda122d2a2f6f576e0cfbad
Huang Jian <huang.jian@howtimeflies.io>
2018-02-12 19:09:11 +0800

    Export all necessary classes to the distribution.

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,4 @@
 export * from './src/app/highcharts/highcharts.module'
+export * from './src/app/highcharts/highcharts.config'
+export * from './src/app/highcharts/highcharts.service'
+export * from './src/app/highcharts/highchart.component'
