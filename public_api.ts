1878776c3497690c49731773fa57f0d3ffc58c2f
Huang Jian <huang.jian@howtimeflies.io>
2018-02-20 18:14:51 +0800

    Export the helper functions.

--- public_api.ts
+++ public_api.ts
@@ -3,3 +3,6 @@ export * from './src/app/highcharts/highcharts-testing.module'
 export * from './src/app/highcharts/highcharts.config'
 export * from './src/app/highcharts/highcharts.service'
 export * from './src/app/highcharts/highchart.component'
+
+export * from './src/app/helper/helper'
+export * from './src/app/helper/lazy-asset-loader'
